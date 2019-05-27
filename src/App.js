13af8952f24bb507e133ea32e87a5dc6d94dcf22
AxelE <axel91evrard@gmail.com>
2019-05-27 16:47:28 +0200

    modify a thing

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Axel was here
         </a>
       </header>
     </div>
