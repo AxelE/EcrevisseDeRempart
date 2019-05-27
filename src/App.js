79e770743099bc0dd836152f6252c834c838115b
AxelE <axel91evrard@gmail.com>
2019-05-28 01:34:57 +0200

    Travis madman

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Tryin to master travis
+          Travis turn me mad
         </a>
       </header>
     </div>
