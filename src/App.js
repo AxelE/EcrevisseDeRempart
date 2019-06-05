769ade9dc67a27c51ddd12f585cf1a5a627123c2
AxelE <axel91evrard@gmail.com>
2019-06-05 15:26:15 +0200

    Change presentation and README

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hey EcrevisseDeRempart, thx to visit my test website.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/AxelE/EcrevisseDeRempart"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Travis turn me mad
+          Link to the github
         </a>
       </header>
     </div>
